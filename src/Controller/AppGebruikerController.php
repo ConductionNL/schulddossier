036ea29b2c6b4fa511117a9746e8e3779ec510a2
Bart Elferink <bart@soepel.it>
2021-07-12 10:15:47 +0200

    issue #252

--- src/Controller/AppGebruikerController.php
+++ src/Controller/AppGebruikerController.php
@@ -34,7 +34,7 @@ class AppGebruikerController extends Controller
         $pagination = $paginator->paginate(
             $repository->generatePaginationQueryForUser($this->getUser(), $inactive),
             $request->query->getInt('page', 1),
-            10
+            20
         );
 
         $onbekendeGebruikers = $repository->findAllOnbekendeGebruikers()->getQuery()->getResult();
