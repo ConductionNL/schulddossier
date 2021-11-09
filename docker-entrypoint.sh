b59203fe815b426a33c6867813822ef63cbb2a4e
ginokok1996 <ginokok1996@hotmail.com>
2021-11-09 15:09:05 +0100

    entrypoint update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,11 +15,9 @@ cat /etc/hosts
 
 composer run-script post-install-cmd
 
-if [ "$APP_ENV" = "dev" ]
-then
-    composer install --no-interaction --no-suggest --prefer-dist
-    # no cache:warmup needed for dev
-elif [ "$APP_ENV" = "production" ]
+composer install --prefer-dist --no-progress --no-suggest --no-interaction
+
+if [ "$APP_ENV" = "production" ]
 then
     # composer install already executed in Dockerfile
     php bin/console cache:warmup --env=production
